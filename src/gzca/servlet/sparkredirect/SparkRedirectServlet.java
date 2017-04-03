4ec79bd147fea522413ce073cf5709a807dbd7ab
wuziqingwzq <493444501@qq.com>
2017-04-03 15:22:41 +0800

    Rejected commit: Default

--- src/gzca/servlet/sparkredirect/SparkRedirectServlet.java
+++ src/gzca/servlet/sparkredirect/SparkRedirectServlet.java
@@ -1,5 +1,8 @@
 package gzca.servlet.sparkredirect;
 
+import cn.com.syan.spark.app.sdk.connect.SparkConnectException;
+import cn.com.syan.spark.app.sdk.connect.oauth.Oauth;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +13,19 @@ import java.io.IOException;
 /**
  * Created by wuziqing on 2017-4-3.
  */
-@WebServlet(name = "SparkRedirectServlet")
+
+@WebServlet(name = "SparkRedirectServlet",urlPatterns = "/SparkRedirectServlet")
 public class SparkRedirectServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        System.out.println("testservlet");
+        if(request.getParameter("type")=="login"){
+            try {
+                String authURL = new Oauth().getAuthorizeURL(request);
+                response.sendRedirect(authURL);
+            } catch (SparkConnectException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
